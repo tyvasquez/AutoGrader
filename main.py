09b7fa968213993b1185009e8bdd03a33f2b5f54
tyvasquez <56778652+tyvasquez@users.noreply.github.com>
2022-05-02 21:32:44 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
+#Importing of other classes
 import Assignment as assignment
 import Grader as grader
 import Output as output
 
 #These are the values for the assignment
+
 q1 = assignment.max([1,8,3])
 q2 = assignment.length('Hello World')
 q3 = assignment.palindrome('racecar')
+q4 = assignment.closest([10,3,7,8,15],4)
 
 #Our expected output for first question
 with open('ExpectedOutputs.txt') as f:
@@ -19,13 +21,20 @@ with open('ExpectedOutputs.txt' )as f:
 with open('ExpectedOutputs.txt')as f:
     exp3 = f.readlines()[2]
 
+with open('ExpectedOutputs.txt')as f:
+    exp4 = f.readlines()[3]
+
+
+
 #Calls the Grader Class
-a = grader.Grader(q1,q2,q3,exp1,exp2,exp3,0)
-grade = (a.checkMatching(q1,q2,q3,exp1,exp2,exp3))
+a = grader.Grader(q1,q2,q3,q4,exp1,exp2,exp3,exp4,0)
+grade = (a.checkMatching(q1,q2,q3,q4,exp1,exp2,exp3,exp4))
 
 #Calls the Output Class
 b = output.Output(grade)
 final = (b.printGrade(grade))
+
+
 
 
 
