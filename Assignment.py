52ad87147138baa39b765d83e2ae3573851563e3
tyvasquez <56778652+tyvasquez@users.noreply.github.com>
2022-05-02 21:32:22 -0400

    Add files via upload

--- Assignment.py
+++ Assignment.py
@@ -1,9 +1,9 @@
-#This is the student's assignment. They are given the predetermined array
-#and must find the largest number in the array
-
+#This is the student's assignment. They are given 3 questions which
+# they must complete by the guidelines set in the instructions
 
 #Question one:
-#Create a function called max which takes a given array [1,8,3] and returns the maximum value in that array
+#Create a function called max which takes a given array [1,8,3] and
+# returns the maximum value in that array
 import numpy as np
 
 def max(a):
@@ -14,7 +14,8 @@ a = [1,8,3]
 max(a)
 
 #Question two:
-#Create a function called length that returns the length of the string 'Hello World'
+#Create a function called length that returns the
+# length of the string 'Hello World'
 
 def length(a):
     length  = (len(a))
@@ -24,8 +25,10 @@ a = 'hello world'
 length(a)
 
 #Question 3:
-#Write a recursive fuction called palindrome that gets the string 'racecar' and returns True if it is a palindrome and False otherwise
-#A palindrome is a string that reads the same forward as backward, e.g., level , radar ,racecar
+#Write a recursive fuction called palindrome that gets the
+# string 'racecar' and returns 'True' if it is a palindrome and False otherwise
+#A palindrome is a string that reads the
+# same forward as backward, e.g., level , radar ,racecar
 
 def palindrome(a):
     length = len(a)
@@ -37,3 +40,16 @@ def palindrome(a):
         return False
 a = 'racecar'
 palindrome(a)
+#Question4
+# Write a function that gets an 1-D array a and a number v, and returns the closest value
+# in the array to v. For example, if a = [10, 3, 7, 8, 15] and v = 4, then the function should
+# return 3. Hint: use the function np.argmin().
+
+import numpy as np
+def closest(b,v):
+    array = np.array(b)
+    x = np.abs(array-v).argmin()
+    return (array[x])
+b = [10,3,7,8,15]
+v =4
+x = closest(b,v)
--- Output.py
+++ Output.py
@@ -6,5 +6,10 @@ class Output:
         self.grade = grade
 
     def printGrade(self,grade):
+        '''
+        generates a file and writes the students grade to that external text file
+        :param grade:
+        :return:
+        '''
         with open('Grades.txt' , 'w') as f:
             f.write('Student one scored ' + str(self.grade) + '% on the assignment \n')
